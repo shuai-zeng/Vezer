2874cc7eb95006532947b6006a6dffffb5c92f20
Shuai Zeng <zengshuai1995@gmail.com>
2017-02-17 17:15:21 +0000

    - Added responsive chart to display the trends (temperature by default) - Text views are clickable and display the corresponding trends

--- app/src/main/java/com/azhei/vezer/MainActivityFragment.java
+++ app/src/main/java/com/azhei/vezer/MainActivityFragment.java
@@ -28,6 +28,17 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import java.util.ArrayList;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -41,12 +52,12 @@ public class MainActivityFragment extends Fragment {
     private static String TAG = MainActivity.class.getSimpleName();
 
     private TextView tempValue;
+    private LineChart lineChart;
     private TextView humValue;
     private TextView rainValue;
     private TextView pressValue;
-    private ProgressDialog pDialog;
 
-    private String jsonResponse;
+    private ProgressDialog pDialog;
 
     public MainActivityFragment() {
     }
@@ -57,6 +68,7 @@ public class MainActivityFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
         tempValue = (TextView) v.findViewById(R.id.tempValue);
+        lineChart = (LineChart) v.findViewById(R.id.lineChart);
         humValue = (TextView) v.findViewById(R.id.humValue);
         rainValue = (TextView) v.findViewById(R.id.rainValue);
         pressValue = (TextView) v.findViewById(R.id.pressValue);
@@ -65,14 +77,41 @@ public class MainActivityFragment extends Fragment {
         pDialog.setMessage("Please wait...");
         pDialog.setCancelable(false);
 
+        requestWeather(tempURL);
+        requestWeather(humURL);
+        // requestWeather(rainURL);
+        requestWeather(pressURL);
+
+        requestChart(tempURL);
         Button refresh = (Button) v.findViewById(R.id.refresh);
         refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 requestWeather(tempURL);
                 requestWeather(humURL);
-                requestWeather(rainURL);
+                // requestWeather(rainURL);
                 requestWeather(pressURL);
+            }
+        });
+
+        tempValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestChart(tempURL);
+            }
+        });
+
+        humValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestChart(humURL);
+            }
+        });
+
+        pressValue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestChart(pressURL);
             }
         });
 
@@ -88,27 +127,10 @@ public class MainActivityFragment extends Fragment {
                 Log.d(TAG, response.toString());
 
                 try {
-
                     JSONObject jsonResponse = (JSONObject) response.get(0);
-
                     String payload = jsonResponse.getString("payload");
 
-                    float payloadValue = Float.valueOf(payload);
-
-                    switch (reqURL) {
-                        case "http://192.168.0.13:3000/api/temps":
-                            tempValue.setText(payload + " °C");
-                            break;
-                        case "http://192.168.0.13:3000/api/hums":
-                            humValue.setText(payload + " %");
-                            break;
-                        case "http://192.168.0.13:3000/api/rains":
-                            rainValue.setText(payload + " %");
-                            break;
-                        case "http://192.168.0.13:3000/api/press":
-                            pressValue.setText(payload + "kPa");
-                            break;
-                    }
+                    loadData(payload, reqURL);
 
                 } catch (JSONException e) {
                     Toast.makeText(getActivity(),
@@ -130,6 +152,107 @@ public class MainActivityFragment extends Fragment {
         AppController.getInstance().addToRequestQueue(jsonObjReq);
     }
 
+    private void requestChart(final String reqURL) {
+        showpDialog();
+
+        JsonArrayRequest jsonObjReq = new JsonArrayRequest(Method.GET, reqURL, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
+
+                try {
+                    String[] payload = new String[response.length()];
+
+                    for (int i = 0; i < response.length(); i++){
+                        JSONObject jsonResponse = (JSONObject) response.get(i);
+                        payload[i] = jsonResponse.getString("payload");
+                    }
+
+                    loadChart(payload, reqURL);
+
+                } catch (JSONException e) {
+                    Toast.makeText(getActivity(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+
+                hidepDialog();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                hidepDialog();
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
+    }
+
+    private void loadData (String data, String URL){
+
+        switch (URL) {
+            case "http://192.168.0.13:3000/api/temps":
+                tempValue.setText(data + " °C");
+                break;
+            case "http://192.168.0.13:3000/api/hums":
+                humValue.setText(data + " %");
+                break;
+            case "http://192.168.0.13:3000/api/rains":
+                rainValue.setText(data + " %");
+                break;
+            case "http://192.168.0.13:3000/api/press":
+                pressValue.setText(data + "kPa");
+                break;
+        }
+    }
+
+    private void loadChart(String[] data, String URL){
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        for(int i = 0; i < data.length; i++) {
+            entries.add(new Entry(i, Float.valueOf(data[data.length - 1 - i])));
+        }
+
+        String labeltag = "";
+
+        switch (URL) {
+            case "http://192.168.0.13:3000/api/temps":
+                tempValue.setText(data[0] + " °C");
+                labeltag = "Temperature";
+                break;
+            case "http://192.168.0.13:3000/api/hums":
+                humValue.setText(data[0] + " %");
+                labeltag = "Humidity";
+                break;
+            case "http://192.168.0.13:3000/api/rains":
+                rainValue.setText(data[0] + " %");
+                labeltag = "Rain Index";
+                break;
+            case "http://192.168.0.13:3000/api/press":
+                pressValue.setText(data[0] + "kPa");
+                labeltag = "Pressure";
+                break;
+        }
+
+        LineDataSet dataset = new LineDataSet(entries, labeltag);
+        dataset.setDrawValues(false);
+        LineData linedata = new LineData(dataset);
+        lineChart.setData(linedata);
+
+        setChartStyle();
+    }
+
+    private void setChartStyle() {
+        lineChart.setDescription(null);
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.setEnabled(false);
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setEnabled(false);
+
+        lineChart.invalidate();
+    }
 
     private void showpDialog() {
         if (!pDialog.isShowing())
